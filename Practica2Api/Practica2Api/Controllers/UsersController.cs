1b3f3f6ed856582135b752bf407333f98a6f77e6
SantiagoTorrico <Santiago.Torrico@ucb.edu.bo>
2022-04-08 17:07:52 -0400

    Created Users

---
+++ Practica2Api/Practica2Api/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica2Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostUsers()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutUsers()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteUsers()
+        {
+            return Ok();
+        }
+    }
+}
